9df8b80554f75263f3f780230efaf9debbf39298
Esteban Barajas <estebangit@users.noreply.github.com>
2018-11-10 10:38:44 +0100

    Upgrade to 2.1.0

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -1,14 +1,25 @@
 package hello;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
-    
+
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
     }
-    
+
+    @RequestMapping(value = "/test/{id}", method = RequestMethod.GET)
+    public String test(@PathVariable("id") String id) {
+        return "This is a test with id " + id;
+    }
+
+    @GetMapping("/test2/{id}")
+    public ResponseEntity<?> getTest2(@PathVariable String id) {
+        return new ResponseEntity<>("This is a test2 with id " + id, HttpStatus.OK);
+    }
+
 }
